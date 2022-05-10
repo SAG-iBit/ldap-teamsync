02e291b993e9c842d918529678e57bb4ed0b8757
Kalina Buhleva <Kalina.Buhleva@softwareag.com>
2022-05-10 14:23:40 +0300

    Revert "Add remove member parameter"
    This reverts commit ffbd388145a0496dfb403d0fd4e0b903d9f5318c.

--- app.py
+++ app.py
@@ -19,8 +19,6 @@ from githubapp import (
     TEST_MODE,
     USER_SYNC_ATTRIBUTE,
     SYNCMAP_ONLY,
-    ADD_MEMBER,
-    REMOVE_MEMBER,
 )
 
 app = Flask(__name__)
@@ -198,23 +196,21 @@ def execute_sync(org, team, slug, state):
     else:
         for user in state["action"]["add"]:
             # Validate that user is in org
-<<<<<<< HEAD
-            if org.is_member(user) or ADD_MEMBER:
-                try:
-                    print(f"Adding {user} to {slug}")
-                    team.add_or_update_membership(user)
-                except github3.exceptions.NotFoundError:
-                    print(f"User: {user} not found")
-                    pass
-                except Exception as e:
-                    print(f"DEBUG: {e}")
-                    traceback.print_exc(file=sys.stderr)
-                    pass
+            # if org.is_member(user) or ADD_MEMBER:
+            try:
+                print(f"Adding {user} to {slug}")
+                team.add_or_update_membership(user)
+            except github3.exceptions.NotFoundError:
+                print(f"User: {user} not found")
+                pass
+            except Exception as e:
+                print(f"DEBUG: {e}")
+                traceback.print_exc(file=sys.stderr)
+                pass
 
         for user in state["action"]["remove"]:
-            if org.is_member(user) or REMOVE_MEMBER:
-                print(f"Removing {user} from {slug}")
-                team.revoke_membership(user)
+            print(f"Removing {user} from {slug}")
+            team.revoke_membership(user)
 
 
 def open_issue(client, slug, message):
